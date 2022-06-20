449d7423c24e8b5baa124f12ddb8deb123b1fa86
Tharindu Wijetunge <79456617+TWijetunge@users.noreply.github.com>
2022-06-20 16:48:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,146 +1,270 @@
-//const joi=require('joi'); // 'joi' is capital because, joi returns a class 
-const express = require('express');  // it returns a 'function'
-const app=express(); // this is how we call that function
-                     // express() creates an application and it is now assign to our 'app' constant
+const express =require("express");
+const app = express();
+const bodyParser= require("body-parser");
+const cors= require("cors");
+const mysql = require("mysql");
 
 
-//const dbconfig= require('./dbconfig');               
 
-//app.use(express.json()); 
-const mysql=require('mysql');
+const db = mysql.createPool({
+    host:"localhost",
+    user: "root",
+    password:"Tharindu@mysql1999",
+     database: "Employee_Management_System"})
 
-const connect= mysql.createConnection(
-    {
-        "host": "localhost",
-        "user": "root",
-        "password":"Tharindu@mysql1999",
-        "port": "3306",
-        "database": "Employee_Management_System"
-    }
-);
- connect.connect((error)=>{
-     if(error){
-         console.log("connection faild");
-     }
-     else {
-         console.log("Database connected");
-     }
-     
- })
 
-app.get('/login', (req,res)=>{
-    connect.query("select Username, Password from Account", (error, result)=>{
-        if(error){
-            console.log(error);
-        }
 
-        else {
-            res.send(result);
-        }
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+//-------------------------------Tharindu Wijetunge----------------------------------
+app.get("/api", (req,res)=>{
+    const sqlget=`select Username from Account where Password= "1234"`;
+    db.query(sqlget, (error,result)=>{
+        res.send(result);
     })
-});
+})
 
+app.post("/api/login",(req,res)=>{
+    let uname = req.body.username;
+    let psword = req.body.password;
+    const sqlpost = `select 
+                     User_.userID,
+                     Account.Username, 
+                     Account.Password, 
+                     Account.type, 
+                     User_.userName, 
+                     User_.Email, 
+                     User_.address 
+                     from User_ cross join Account
+                     on 
+                     Account.Username='${uname}' and Account.Password='${psword}'
+                     and
+                     User_.userID=Account.UID`;
 
-app.get('/profile/users/:id', (req,res)=>{
-    let uid=req.params.id;
-    connect.query(`select LoanID, Amount,description,loandate,loanstatus from Loan where userID= ${uid}`, (error, result)=>{
-        if(error){
-            console.log(error);
+    db.query(sqlpost,(error,result)=>{
+
+        result = JSON.parse(JSON.stringify(result));
+        if(result[0]!=null){
+            if(result[0].Username==uname && result[0].Password==psword){
+                res.send({
+                    message:"successfull",
+                    status:true,
+                    type: result[0].type,
+                    name:result[0].userName,
+                    email:result[0].Email,
+                    address: result[0].address,
+                    uid:result[0].userID
+                })
+            }
+
+            else{
+                res.send({
+                    message:"unsuccessfull"
+                })
+
+            }
+
         }
         else{
-            res.send(result);
+            res.send({
+                message:"user not found"
+            })
         }
     })
-
 });
 
-app.get('/profile/users/:id/processed-by', (req,res)=>{
-    let uid=req.params.id;
-    connect.query(`select L.LoanID, L.description, U.userName from User_ as U, Loan as L
-    where L.HRID= U.userID and L.userID= ${uid}`, (error, result)=>{
-        if(error){
-            console.log(error);
-        }
-        else{
-            res.send(result);
-        }
+app.post('/api/profile/update/:uid_',(req,res)=>{
+    const {uid_}=req.params;
+    
+
+    const sqlpost = `update User_ set userName='${req.body.name}' ,Email='${req.body.email}', address='${req.body.address}' where userID='${uid_}'`;
+    db.query(sqlpost,(error,result)=>{
+        res.send({
+            message:"successful"
+        })
     })
-
 });
 
+app.post('/profile/loan', (req,res)=>{
+    let uid= req.body.userID;
+    let amount= req.body.amount;
+    let description= req.body.description;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const courses=[
-    {id:1,name:'course1'},
-    {id:2,name:'course2'},
-    {id:3,name:'course3'}
-
-]
-*/
-
-/*app.get('/', (req, res)=>{
-    res.send('Helloooooo.....');
+    const loanpost=`insert into 
+    Loan  (Amount,description, loandate,loanstatus,remarks,userID ,HRID )
+    values( '${amount}','${description}',curdate(), "pending","null", '${uid}',null);`;
+    db.query(loanpost, (error,result)=>{
+        res.send({
+            message:"successful"
+        })
+    })
 });
 
-app.get('/api/courses', (req,res)=>{
-    res.send(courses);
-});*/
+app.post('/profile/leave',(req,res)=>{
+    let userid = req.body.uid;
+    let description = req.body.description;
+    let leavetype = req.body.leavetype;
+    let fromdate = req.body.fromdate;
+    let todate = req.body.todate;
+
+    const leavepost=`insert into Leave_(LeaveType,description,fromdate,todate,userID,ManagerID) values('${leavetype}','${description}','${fromdate}','${todate}',"pending",'${userid}',null)`;
+    db.query(leavepost,(error,result)=>{
+        res.send({
+            message:"successful"
+        })
+    })
+});
+
+app.post('/profile/attendance',(req,res)=>{
+    let  uID = req.body.userid;
+    const attendancepost=`call attendance('${uID}',@marked);
+    select @marked;`;
+    db.query(attendancepost,(error,result)=>{
+        res.send({
+            message:"successfull"
+        })
+    })
+})
+
+//-----------------------------------------------------------------------------
+
+//-------------------------Sahanya Ranaweera--------------------------------------------
+
+// create user
+app.post('/create', (req, res) => {
+    let LastName = req.body.LastName;
+    let FirstName = req.body.FirstName;
+    let Address = req.body.Address;
+    let City = req.body.City;
+  
+    // validation
+    if (!LastName || !FirstName)
+      return res.status(400).send({
+        error: true,
+        message: "Please provide first name and last name",
+      });
+  
+    // insert to db
+    db.query(
+      "INSERT INTO users (LastName, FirstName, Address, City) VALUES (?, ?, ?, ?)",
+      [LastName, FirstName, Address, City],
+      function (error, results, fields) {
+        if (error) throw error;
+        return res.send({
+          error: false,
+          data: results,
+          message: "User successfully added",
+        });
+      }
+    );
+  });
 
 
-/*app.get('/api/courses/:id', (req,res)=>{
-    const course=courses.find(c=>c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given id wansnt found');
-    res.send(course);
-});*/
+
+//get all users------------------
+  app.get('', (req, res) => {
+    db.query("SELECT * FROM users", function (error, results, fields) {
+      if (error) throw error;
+  
+      // check has data or not
+      let message = "";
+      if (results === undefined || results.length == 0)
+        message = "Users table is empty";
+      else message = "Successfully retrived all users";
+  
+      return res.send({ error: false, data: results, message: message });
+    });
+  });
 
 
-/*app.post('/api/courses', (req,res)=>{
+// get user by id----------------------
+  app.get('/:id', (req, res) => {
+    let userID = req.params.id;
 
+    db.query("SELECT * FROM users where userID = ?",
+    [userID], function (error, results, fields) {
+      if (error) throw error;
+  
+      // check has data or not
+      let message = "";
+      if (results === undefined || results.length == 0)
+        message = "No user with that ID";
+      else message = "Successfully retrived user data";
+  
+      return res.send({ error: false, data: results, message: message });
+    });
+  });
 
-    const schema= {
-        name: joi.string().min(3).required()
-    };
-
-    const result=new joi.ValidationError(req.body,schema);
-
-
-     
-    if(!req.body.name||req.body.name.length<3){
-        //bad request
-        res.status(400).send('Name is needed and length must be greatervthan 3 charachters');
-       return;
+  
+// update user with id----------------------------------
+  app.patch('/:id', (req, res) => {
+    let userID = req.params.id;
+    let LastName = req.body.LastName;
+    let FirstName = req.body.FirstName;
+    let Address = req.body.Address;
+    let City = req.body.City;
+  
+    // validation
+    if (!LastName || !FirstName) {
+      return res.status(400).send({
+        error: true,
+        message: "Please provide user firstname, lastname",
+      });
     }
+  
+    db.query(
+      "UPDATE users SET LastName = ?, FirstName = ?, Address = ?, City = ? WHERE userID = ?",
+      [LastName, FirstName, Address, City, userID],
+      function (error, results, fields) {
+        if (error) throw error;
+  
+        // check data updated or not
+        let message = "";
+        if (results.changedRows === 0)
+          message = "User data not found or data are same";
+        else message = "User data successfully updated";
+  
+        return res.send({ error: false, data: results, message: message });
+      }
+    );
+  });
 
-    const course = {
-        id: courses.length + 1, //when we integrate with database, the database will assign the id
-        name: req.body.name  //as we need to read the name from the body of the request
-    };
-    courses.push(course);  //push it to the array
-    res.send(course);      
-})*/
 
 
-//PORT
-const port =process.env.PORT || 3000;   //here port is dynamically assigned by environment variables.
-app.listen(port, ()=>console.log(`listenning on port ${port}`));
+  app.delete('/:id', (req, res) => {
+    let userID = req.params.id;
+  
+    if (!userID) {
+        return res.status(400).send({ error: true, message: 'Please provide user id' });
+    }
+    db.query('DELETE FROM users WHERE userID = ?', [userID], function (error, results, fields) {
+        if (error) throw error;
+
+        // check data updated or not
+        let message = "";
+        if (results.affectedRows === 0)
+            message = "User not found";
+        else
+            message = "User successfully deleted";
+
+        return res.send({ error: false, data: results, message: message });
+    });
+});
+
+
+//---------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log("Server running on port 5000");
+})
